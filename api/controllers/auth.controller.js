98d5e2007d4be2bb6ceab1d397e96edee32e55ba
Railey Inductivo <80341106+RaileyAnthony@users.noreply.github.com>
2025-05-18 20:32:52 +0400

    login domain onrender

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -41,6 +41,7 @@ export const login = async (req, res, next) => {
         httpOnly: true,
         secure: true,
         sameSite: "None",
+        domain: ".onrender.com",
       })
       .status(200)
       .send(info);
@@ -54,6 +55,7 @@ export const logout = async (req, res) => {
     .clearCookie("accessToken", {
       sameSite: "none",
       secure: true,
+      domain: ".onrender.com",
     })
     .status(200)
     .send("User has been logged out.");
