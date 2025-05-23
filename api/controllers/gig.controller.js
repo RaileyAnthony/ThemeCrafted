4a1d4b4144849cc32208411700b80cc304f82377
Railey Inductivo <80341106+RaileyAnthony@users.noreply.github.com>
2025-05-23 12:07:46 +0400

    gig controller filter update

--- api/controllers/gig.controller.js
+++ api/controllers/gig.controller.js
@@ -44,7 +44,7 @@ export const getGigs = async (req, res, next) => {
   const q = req.query;
   const filters = {
     ...(q.userId && { userId: q.userId }),
-    ...(q.cat && { cat: q.cat }),
+    ...(q.cat && { cat: { $in: q.cat.split(",") } }),
     ...((q.min || q.max) && {
       price: {
         ...(q.min && { $gt: q.min }),
